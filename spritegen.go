f115dd018789a626346ce4abfc676ffbc5e58d32
Jake McGinty <me@jake.su>
2015-05-20 17:29:55 -0700

    support padding between sprites

--- spritegen.go
+++ spritegen.go
@@ -46,24 +46,27 @@ func readResources (emojiXml string) (resources *Resources, err error) {
   return
 }
 
-func getSingleSpriteRect(index int, dimens image.Point, maxWidth int) (rect image.Rectangle) {
-  emojiPerLine := maxWidth / dimens.X
-  x := (index % emojiPerLine) * dimens.X
-  y := index / emojiPerLine * dimens.Y
+func getSingleSpriteRect(index int, dimens image.Point, pad image.Point, maxWidth int) (rect image.Rectangle) {
+  emojiPerLine := maxWidth / (dimens.X + pad.X)
+  line := index / emojiPerLine
+  row := index % emojiPerLine
+  x := row * dimens.X + row * pad.X
+  y := line * dimens.Y + line * pad.Y
   rect = image.Rect(x, y, x + dimens.X, y + dimens.Y)
   return rect
 }
 
-func getSpriteRect(codePoints []CodePoint, dimens image.Point, maxWidth int) (rect image.Rectangle) {
+func getSpriteRect(codePoints []CodePoint, dimens image.Point, pad image.Point, maxWidth int) (rect image.Rectangle) {
   var width        int
   var height       int
-  if len(codePoints) * dimens.X < maxWidth {
-    width  = len(codePoints) * dimens.X
+  if len(codePoints) * (dimens.X + pad.X) < maxWidth {
+    width  = len(codePoints) * dimens.X + (len(codePoints) - 1) * pad.X
     height = dimens.Y
   } else {
-    emojiPerLine := maxWidth / dimens.X
+    emojiPerLine := maxWidth / (dimens.X + pad.X)
+    lines := int(math.Ceil(float64(len(codePoints)) / float64(emojiPerLine)))
     width = emojiPerLine * dimens.X
-    height = int(math.Ceil(float64(len(codePoints)) / float64(emojiPerLine))) * dimens.Y
+    height = lines * dimens.Y + pad.Y * (lines - 1)
   }
   rect = image.Rect(0, 0, width, height)
   return
@@ -89,6 +92,9 @@ func main() {
   var MaxWidth int
   var EmojiWidth int
   var EmojiHeight int
+  var EmojiPadY int
+  var EmojiPadX int
+
   var SpritegenCmd = &cobra.Command{
     Use: "spritegen",
     Short: "Spritegen takes a set of codepoints and emoji assets and generates sprites for them",
@@ -98,15 +104,16 @@ func main() {
         log.Fatal(err)
       }
       emojiDimens := image.Pt(EmojiWidth, EmojiHeight)
+      padDimens := image.Pt(EmojiPadX, EmojiPadY)
       for _, intArray := range resources.IntegerArrays {
-        sprite := image.NewRGBA(getSpriteRect(intArray.CodePoints, emojiDimens, MaxWidth))
+        sprite := image.NewRGBA(getSpriteRect(intArray.CodePoints, emojiDimens, padDimens, MaxWidth))
         for j, codePoint := range intArray.CodePoints {
           emojipath := filepath.Join(EmojiDir, EmojiPrefix + string(codePoint) + ".png")
           m, err := getEmoji(emojipath)
           if err != nil {
             log.Fatal(err)
           }
-          draw.Draw(sprite, getSingleSpriteRect(j, emojiDimens, MaxWidth), m, image.Pt(0, 0), draw.Over)
+          draw.Draw(sprite, getSingleSpriteRect(j, emojiDimens, padDimens, MaxWidth), m, image.Pt(0, 0), draw.Over)
         }
         out, err := os.Create(intArray.Name + ".png")
         if err != nil {
@@ -122,6 +129,8 @@ func main() {
   SpritegenCmd.Flags().StringVarP(&EmojiPrefix, "emoji-prefix", "p", "emoji_u", "Prefix used by emoji files")
   SpritegenCmd.Flags().IntVarP(&EmojiWidth, "emoji-width", "x", 136, "Input width of emoji (they are forced square)")
   SpritegenCmd.Flags().IntVarP(&EmojiHeight, "emoji-height", "y", 128, "input height of emoji (they are forced square)")
+  SpritegenCmd.Flags().IntVarP(&EmojiPadX, "emoji-pad-x", "q", 0, "Horizontal padding of sprited emoji")
+  SpritegenCmd.Flags().IntVarP(&EmojiPadY, "emoji-pad-y", "r", 8, "Vertical padding of sprited emoji")
   SpritegenCmd.Flags().IntVarP(&MaxWidth, "max-width", "m", 2048, "Max width of sprite")
   SpritegenCmd.Execute()
 }
